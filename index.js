f76f26cf8bde3c5e8ebd4c6cbaf6b3ca841e6510
Srushti Sulgudle <68942778+S3-26@users.noreply.github.com>
2021-08-20 18:21:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function inputit(){
     window.alert('dhkd');
    window.location.href="sort.html";
 }
+
+function responsive_menu(){
+    var e=document.getElementById('menu_list');
+    if(e.className === 'navbar1'){
+        e.className += " responsive";
+    }
+    else{
+        e.className='navbar1';
+    }
+}
